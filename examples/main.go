f51473413ae7c81b9fadab232ae264c68a62f1f3
Biswarup Adhikari <biswarupadhikari@gmail.com>
2014-08-30 00:00:36 +0530

    And Where OR Where And Raw Where Implemented and SQL Generate Working

--- examples/main.go
+++ examples/main.go
@@ -1,10 +1,14 @@
 package main 
 import(
-"log"
+//"log"
 "github.com/biswarupadhikari/gomysql"
 )
 func main() {
 	db:=gomysql.Connect("root","rootwdp","go")
-	db.Select("id,username,email")
-	log.Println("HI",db)
+	db.Select("id,username as p,email").From("users").Where("id",">",15)
+	db.Where("username","=","biswarupadhikari")
+	db.ORWhere("id","<",150)
+	db.RawWhere(" NOR password=? ","taste")
+	db.RawWhere(" XOR password=? ",15)
+	db.Get()
 }
--- gomysql.go
+++ gomysql.go
@@ -1,50 +1,116 @@
 package gomysql
-import(
+
+import (
+"database/sql"
+_ "github.com/go-sql-driver/mysql"
 "log"
 "strings"
-"database/sql"
-_"github.com/go-sql-driver/mysql"
 )
+
 /**
  * GoMysql Structure
  */
- type GoMysql struct{	
- 	db *sql.DB
- 	fields []string
- 	tableName string
- 	conditions []string
- 	dataValues []interface{}
+ type GoMysql struct {
+   db         *sql.DB
+   fields     []string
+   tableName  string
+   conditions []string
+   dataValues []interface{}
  }
+
 /**
  * Connect To DataBase
  */
- func Connect(dbUsername string,dbPassword string,dbName string)*GoMysql{
- 	db, err := sql.Open("mysql", dbUsername+":"+dbPassword+"@/"+dbName)
- 	if err != nil {
- 		log.Fatal(err)
- 	}
- 	err = db.Ping()
- 	if err != nil {
- 		log.Fatal(err)
- 	}
- 	gomysql:=new(GoMysql)
- 	gomysql.db=db
- 	return gomysql
- }
+ func Connect(dbUsername string, dbPassword string, dbName string) *GoMysql {
+   db, err := sql.Open("mysql", dbUsername+":"+dbPassword+"@/"+dbName)
+   if err != nil {
+    log.Fatal(err)
+  }
+  err = db.Ping()
+  if err != nil {
+    log.Fatal(err)
+  }
+  gomysql := new(GoMysql)
+  gomysql.db = db
+  return gomysql
+}
+
 /**
  * Select Fields
  */
- func (gomysql *GoMysql)Select(fields string)*GoMysql{
- 	cols:=strings.Split(fields,",")
- 	for _,field:=range cols{
- 		gomysql.fields=append(gomysql.fields,field)
- 	}
- 	return gomysql
- }
- /**
-  * Select From a Table 
-  */
-  func (gomysql *GoMysql)From(tableName string)*GoMysql{
-  	gomysql.tableName=tableName
-  	return gomysql
+ func (gomysql *GoMysql) Select(fields string) *GoMysql {
+   cols := strings.Split(fields, ",")
+   for _, field := range cols {
+    gomysql.fields = append(gomysql.fields, field)
   }
+  return gomysql
+}
+
+/**
+ * Select From a Table
+ */
+ func (gomysql *GoMysql) From(tableName string) *GoMysql {
+   gomysql.tableName = tableName
+   return gomysql
+ }
+
+/**
+ * Add AND Where Conditions
+ */
+ func (gomysql *GoMysql) Where(key string, operator string, dataValue interface{}) *GoMysql {
+   gomysql.applyCondition("`"+key+"`"+operator+" ?", " AND ")
+   gomysql.dataValues = append(gomysql.dataValues, dataValue)
+   return gomysql
+ }
+
+/**
+ * Add OR Where Conditions
+ */
+ func (gomysql *GoMysql) ORWhere(key string, operator string, dataValue interface{}) *GoMysql {
+   gomysql.applyCondition("`"+key+"`"+operator+" ?", " OR ")
+   gomysql.dataValues = append(gomysql.dataValues, dataValue)
+   return gomysql
+ }
+
+/**
+ * Add OR Where Conditions
+ */
+ func (gomysql *GoMysql) RawWhere(condition string, dataValues ...interface{}) *GoMysql {
+   gomysql.conditions = append(gomysql.conditions, condition)
+   for _, dataValue := range dataValues {
+    gomysql.dataValues = append(gomysql.dataValues, dataValue)
+  } 
+  
+  return gomysql
+}
+
+/**
+ * Apply Condition And Add Logical Operator(AND,OR)
+ */
+ func (gomysql *GoMysql) applyCondition(condition string, operatorCondition string) {
+   if len(gomysql.conditions) > 0 {
+    gomysql.conditions = append(gomysql.conditions, operatorCondition+condition)
+  } else {
+    gomysql.conditions = append(gomysql.conditions, condition)
+  }
+}
+
+/**
+ * Generate Select SQL using Select Fields From Table and Condition
+ */
+ func (gomysql *GoMysql) generateSelectSQL() string {
+   var sql string
+   sql += "Select " + strings.Join(gomysql.fields, ",") + " FROM " + gomysql.tableName
+   if len(gomysql.conditions) > 0 {
+    sql += " WHERE " + strings.Join(gomysql.conditions, " ")
+  }
+  return sql
+}
+
+/**
+ * Get Records
+ */
+ func (gomysql *GoMysql) Get() {
+   sql := gomysql.generateSelectSQL()
+   log.Println(sql,gomysql.dataValues)
+ }
