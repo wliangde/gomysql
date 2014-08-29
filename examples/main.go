7e607cf2fb4b33b4c1a57d02c9e100c5262234d2
Biswarup Adhikari <biswarupadhikari@gmail.com>
2014-08-30 01:02:49 +0530

    Insert Query Generate Working

--- examples/main.go
+++ examples/main.go
@@ -5,10 +5,24 @@ import(
 )
 func main() {
 	db:=gomysql.Connect("root","rootwdp","go")
-	db.Select("id,username as p,email").From("users").Where("id",">",15)
-	db.Where("username","=","biswarupadhikari")
-	db.ORWhere("id","<",150)
-	db.RawWhere(" NOR password=? ","taste")
-	db.RawWhere(" XOR password=? ",15)
-	db.Get()
+	/**
+	 * Select Records
+	 */
+	// db.Select("id,username as p,email").From("users").Where("id",">",15)
+	// db.Where("username","=","biswarupadhikari")
+	// db.ORWhere("id","<",150)
+	// db.RawWhere(" NOR password=? ","taste")
+	// db.RawWhere(" XOR password=? ",15)
+	// db.Get()
+	
+	/**
+	 * Insert Record
+	 */
+	data:=make(map[string]interface{})
+	
+	data["username"]="joyantak"
+	data["password"]="ttttt"
+
+	
+	db.Table("users").Insert(data)
 }
