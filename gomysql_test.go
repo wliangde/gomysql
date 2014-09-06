95cf300df6ee0ca50d01469e31effbfbd23daf08
Biswarup Adhikari <biswarupadhikari@gmail.com>
2014-09-06 13:33:49 +0530

    Create Table ,Drop Table and Insert Into Table using Query unit test added

--- gomysql_test.go
+++ gomysql_test.go
@@ -184,3 +184,91 @@ func TestClearWhere(t *testing.T) {
 		t.Error("Clear Where Not Working")
 	}
 }
+
+/**
+ * Test Query Using Create Table
+ */
+func TestQueryCreateTable(t *testing.T) {
+	sqlQuery := `
+    CREATE TABLE IF NOT EXISTS test_users (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    username varchar(250) NOT NULL,
+    password varchar(250) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY username (username)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `
+	_, err := db.Query(sqlQuery)
+	if err != nil {
+		t.Error("Table Create Failed")
+	} else {
+		t.Log("test users Table created or exists")
+	}
+	//Check Table is Exist or not
+	_, err = db.Query("DESC test_users")
+	if err != nil {
+		t.Error("Table not Exist After Create")
+	} else {
+		t.Log("Table Exist")
+	}
+}
+
+/**
+ * Test Query Using Drop Table
+ */
+func TestQueryDropTable(t *testing.T) {
+	sqlQuery := `
+    CREATE TABLE IF NOT EXISTS test_users (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    username varchar(250) NOT NULL,
+    password varchar(250) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY username (username)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `
+	db.Query(sqlQuery)
+	//Drop Table
+	_, err := db.Query("DROP TABLE test_users")
+	if err != nil {
+		t.Error("Error droping Table")
+	} else {
+		t.Log("Table Dropped")
+	}
+	//Confirm Table Dropped or Not
+	_, err = db.Query("DESC test_users")
+	if err != nil {
+		t.Log("Confirmed Table Droped")
+	} else {
+		t.Error("Table Not Dropped Properly")
+	}
+}
+
+/**
+ * Test Query Using Insert Table
+ */
+func TestQueryInsertIntoTable(t *testing.T) {
+	sqlQuery := `
+    CREATE TABLE IF NOT EXISTS test_users (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    username varchar(250) NOT NULL,
+    password varchar(250) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY username (username)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `
+	db.Query(sqlQuery)
+	//Run Insert Query
+	result, err := db.Query("INSERT INTO test_users(id,username,password) VALUES(14599,'biswarupadhikari','secret')")
+	if err != nil {
+		t.Error("Insert Data into test_users table failed")
+	} else {
+		t.Log("Data Inserted into test_users")
+	}
+	newId, _ := result.LastInsertId()
+	if newId != 14599 {
+		t.Error("Insert Data Error insert id not matching")
+	} else {
+		t.Log("Insert id Matching working Insert")
+	}
+
+}
